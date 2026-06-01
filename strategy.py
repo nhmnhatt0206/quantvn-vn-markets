30ebd9cb825b4d68b1d3b05b1490f9d68fa02db1
nhmnhatt0206 <nhmnhatt0206@gmail.com>
2026-06-01 08:36:31 +0700

    Add strategy.py for QuantVN test

--- strategy.py
+++ strategy.py
@@ -1,15 +1,5 @@
-# Khởi tạo client và import các module
-import os
-from dotenv import load_dotenv
-load_dotenv()  # đọc .env vào Python
-from quantvn import client
 import pandas as pd
 import numpy as np
-from quantvn.vn.data import get_derivatives_hist
-from quantvn.vn.metrics import Backtest_Derivates
-client(apikey=os.getenv("QUANTVN_API_KEY"))
-
-df = get_derivatives_hist("VN30F1M", "1m")
 def gen_position(df: pd.DataFrame) -> pd.DataFrame:
     # Copy dữ liệu gốc
     df = df.copy()
@@ -27,8 +17,3 @@ def gen_position(df: pd.DataFrame) -> pd.DataFrame:
     df["position"] = df["position"].fillna(0)
 
     return df
-
-df_pos = gen_position(df)
-print(df_pos)
-backtest = Backtest_Derivates(df_pos, pnl_type="raw")
-backtest.PNL()
